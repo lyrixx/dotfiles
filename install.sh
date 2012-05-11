f9c2e71029f72c044cac80c09890a05a2e9fba4d
Grégoire Pineau <lyrixx@lyrixx.info>
2012-05-11 23:09:33 +0200

    Refactored Bash + added install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CURRENT=`pwd`
+
+ln -sf $CURRENT/.bash_aliases        ~/.bash_aliases
+ln -sf $CURRENT/.bash_common         ~/.bash_common
+ln -sf $CURRENT/.bash_logout         ~/.bash_logout
+ln -sf $CURRENT/.gitconfig           ~/.gitconfig
+ln -sf $CURRENT/.gitglobalexclude    ~/.gitglobalexclude
+ln -sf $CURRENT/.profile             ~/.profile
+ln -sf $CURRENT/.screenrc            ~/.screenrc
+
+if [ $EUID -ne 0 ]; then
+    ln -sf $CURRENT/.bashrc              ~/.bashrc
+else
+    ln -sf $CURRENT/.bashrc_root         ~/.bashrc
+fi
+
+. ~/.bashrc
