1fe68140030572be244e6a6f951f852670ffbb65
Grégoire Pineau <lyrixx@lyrixx.info>
2013-07-05 18:49:00 +0200

    [Git] Fixed git-template installation

--- install.sh
+++ install.sh
@@ -26,7 +26,9 @@ ln -sf $DOTFILES/.profile             ~/.profile
 ln -sf $DOTFILES/.screenrc            ~/.screenrc
 ln -sf $DOTFILES/.tmux.conf           ~/.tmux.conf
 ln -sf $DOTFILES/.vimrc               ~/.vimrc
-ln -sf $DOTFILES/git-template         ~/.git-template
+if [[ ! -d ~/.git-template ]]; then
+    ln -sf $DOTFILES/git-template ~/.git-template
+fi
 
 echo 'Install git submodule'
 if type -t git &> /dev/null; then
